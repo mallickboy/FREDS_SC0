4a154af0897e11a2f57d15514b65992a2fcadbc6
mallickboy <be0safe0in0internet@gmail.com>
2023-08-12 23:03:53 +0530

    Added widget for profile ,name & post showing

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,21 +1,94 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Sidebar from "./Sidebar";
 import Feed from "./Feed.js";
 import Widgets from "./Widgets.js";
 import Profile from './slidebarPages/Profile';
 import Explore from './slidebarPages/Explore';
-// import "./App.css"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+
+import { ethers } from "ethers"
+import { contract_abi, contract_address } from "./Consts/constants";
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+await provider.send("eth_requestAccounts", []);
+const signer = provider.getSigner();
+const contract = new ethers.Contract(contract_address, contract_abi, signer); // Replace with your Ethereum node provider
+const socialMediaContract = contract;
+const pubAddress = await signer.getAddress();
+// console.log("Home = "+pubAddress) 
+// alert(pubAddress)
 
 function Home() {
+
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState({ heading: "", body: "" ,imageFile:""});
+ 
+  // Function to fetch all posts from the smart contract
+  const fetchPosts = async () => {
+    const allPosts = await socialMediaContract.ReturnPosts();
+    setPosts(allPosts);
+    console.log(allPosts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // Function to create a new post
+  const createPost = async () => {
+    const { heading, body , imageFile} = newPost;
+    const userId = "user1"; // Replace with the actual user id
+    try {
+     console.log("imageFileNmae:"+imageFile)
+     
+      await socialMediaContract.addPost( heading, body,imageFile);
+      setNewPost({ heading: "", body: "" });
+      fetchPosts();
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
+  // Function to handle comment submission
+  const handleComment = async (index, message) => {
+    try {
+      await socialMediaContract.comment(index, message);
+      fetchPosts();
+    } catch (error) {
+      console.error("Error adding comment:", error);
+    }
+  };
+  // Function to handle upvote
+  const handleUpvote = async (index) => {
+    try {
+      await socialMediaContract.upvote(index);
+      fetchPosts();
+    } catch (error) {
+      console.error("Error upvoting:", error);
+    }
+  };
+
+  // Function to handle downvote
+  const handleDownvote = async (index) => {
+    try {
+      await socialMediaContract.downvote(index);
+      fetchPosts();
+    } catch (error) {
+      console.error("Error downvoting:", error);
+    }
+  };
+
+
+// modify for hot list widget  / highest post first
+// const sortedPost=posts.sort((a,b)=>
+//     a.upvote - b.upvote     )
+//  let sortedPost=posts;
     return (
-        <div className="app">
+        <div className="app">   
             {/* Sidebar */}
             <Sidebar />
 
             <Routes>
                 {/* <Route path="/" element={<Feed />} />               */}
-                <Route path="/" element={<><Feed/><Widgets/></>} />     
+                <Route path="/" element={<><Feed/><Widgets allPosts={posts}/></>} />     
              
              {/* Profile */}
                 <Route path="/profile" element={<><Profile/></>} /> 
--- frontend/src/Widgets.js
+++ frontend/src/Widgets.js
@@ -1,12 +1,83 @@
-import React from 'react'
 import "./widgets.css"
+import React, { useState, useEffect } from "react";
+import "./feed.css"
+import Post from './Post';
+import Tweetbox from "./TweetBox.js";
+import { Avatar } from '@material-ui/core';
+import FlipMove from "react-flip-move";
 
-function Widgets() {
+import { ethers } from "ethers"
+import { contract_abi, contract_address } from "./Consts/constants";
+import { red } from "@mui/material/colors";
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+await provider.send("eth_requestAccounts", []);
+const signer = provider.getSigner();
+const contract = new ethers.Contract(contract_address, contract_abi, signer); // Replace with your Ethereum node provider
+const socialMediaContract = contract;
+const pubAddress = await signer.getAddress();
+console.log(pubAddress)
+
+
+function Widgets({ allPosts }) {
+
+
+
+
+  let pic = {
+    // image: "https://upload.wikimedia.org/wikipedia/commons/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg",
+    avatar: "https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1920,c_limit/phonepicutres-TA.jpg",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+  }
+  let username = "Tamal", heading = "this is a heading", message = "hellow";
+
   return (
-    <div className='widgets'>
-      <h2>Widgets</h2>
+    <div className='feed' id="widget">
+      <div className="feed__header">
+        <h2>Widgets</h2>
+      </div>
+
+      <div id="widgetTop">
+        <h2> Hot List</h2>
+
+
+        {/* // add post heading here  */}
+<div  id="eachPost" >
+{allPosts.map((eachPost, index) => (
+          // Posts 
+          < div className="post">
+            <div className="post__avatar">
+              <Avatar src={pic.avatar} />
+            </div>
+            <div className='post__header'>
+              <div className='post__headerText'>
+                <h2>
+                  {eachPost.name}{" "} <span> </span>
+                </h2>
+              </div>
+              <div className='post__headerText'>
+                <h3>
+                  {eachPost.heading}{" "} <span> </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+
+        ))};
+</div>
+
+
+
+        {/* // ending post heading here  */}
+
+
+      </div>
+      <div id="widgetBottom" className="widgetBottom">
+        <h2> widgetBottom</h2>
+
+      </div>
     </div>
   )
 }
+
 
 export default Widgets
